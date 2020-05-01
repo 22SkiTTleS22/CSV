5f3d5a86944fb60b7e82be2795c57eee94a48e27
drilya <dr.iliy220599@gmail.com>
2020-05-01 13:52:36 +0400

    Upd - fail() in tests

--- src/test/java/TestMyClass.java
+++ src/test/java/TestMyClass.java
@@ -13,6 +13,8 @@ import static org.junit.Assert.*;
 public class TestMyClass {
 
     private static final String PATH = "./src/main/resources/";
+    private static final String FAIL_MESSAGE_FILESORT = "Exception with fileSort method";
+    private static final String FAIL_MESSAGE_FILES_PROCESSING = "Exception with files processing";
 
     @BeforeClass
     public static void generateFile() throws IOException {
@@ -23,12 +25,17 @@ public class TestMyClass {
     public void testFileSortDescending() {
         try {
             File file = new File(PATH + "alphabet.csv");
-            File methodResult = MyClass.fileSort(file, Comparator.reverseOrder(), 6);
+            File methodResult = null;
+            try {
+                methodResult = MyClass.fileSort(file, Comparator.reverseOrder(), 6);
+            } catch (IOException ex) {
+                fail(FAIL_MESSAGE_FILESORT);
+            }
             File result = new File(PATH + "alphabetDescending.csv");
             assertTrue("The files differ alphabetDescending!", FileUtils.contentEquals(methodResult, result));
             Files.delete(methodResult.toPath());
         } catch (IOException ex) {
-            fail();
+            fail(FAIL_MESSAGE_FILES_PROCESSING);
         }
     }
 
@@ -36,12 +43,17 @@ public class TestMyClass {
     public void testFileSortAscending() {
         try {
             File file = new File(PATH + "alphabet.csv");
-            File methodResult = MyClass.fileSort(file, Comparator.naturalOrder(), 6);
+            File methodResult = null;
+            try {
+            methodResult = MyClass.fileSort(file, Comparator.naturalOrder(), 6);
+            } catch (IOException ex) {
+                fail(FAIL_MESSAGE_FILESORT);
+            }
             File result = new File(PATH + "alphabetAscending.csv");
             assertTrue("The files differ alphabetAscending!", FileUtils.contentEquals(methodResult, result));
             Files.delete(methodResult.toPath());
         } catch (IOException ex) {
-            fail();
+            fail(FAIL_MESSAGE_FILES_PROCESSING);
         }
     }
 
@@ -49,12 +61,17 @@ public class TestMyClass {
     public void testFileSortMultiple() {
         try {
             File file = new File(PATH + "data.csv");
-            File methodResult = MyClass.fileSort(file, Comparator.naturalOrder(), 5);
+            File methodResult = null;
+            try {
+            methodResult = MyClass.fileSort(file, Comparator.naturalOrder(), 5);
+            } catch (IOException ex) {
+                fail(FAIL_MESSAGE_FILESORT);
+            }
             File result = new File(PATH + "result.csv");
             assertTrue("The files differ result Multiple!", FileUtils.contentEquals(methodResult, result));
             Files.delete(methodResult.toPath());
         } catch (IOException ex) {
-            fail();
+            fail(FAIL_MESSAGE_FILES_PROCESSING);
         }
     }
 
@@ -62,12 +79,17 @@ public class TestMyClass {
     public void testFileSortFewer() {
         try {
             File file = new File(PATH + "data.csv");
-            File methodResult = MyClass.fileSort(file, Comparator.naturalOrder(), 20);
+            File methodResult = null;
+            try {
+                methodResult = MyClass.fileSort(file, Comparator.naturalOrder(), 20);
+            } catch (IOException ex) {
+                fail(FAIL_MESSAGE_FILESORT);
+            }
             File result = new File(PATH + "result.csv");
-            assertTrue("The files differ result 2!", FileUtils.contentEquals(methodResult, result));
+            assertTrue("The files differ result Fewer!", FileUtils.contentEquals(methodResult, result));
             Files.delete(methodResult.toPath());
         } catch (IOException ex) {
-            fail();
+            fail(FAIL_MESSAGE_FILES_PROCESSING);
         }
     }
 
@@ -75,12 +97,17 @@ public class TestMyClass {
     public void testFileSortBiggerNoMultiple() {
         try {
             File file = new File(PATH + "data.csv");
-            File methodResult = MyClass.fileSort(file, Comparator.naturalOrder(), 4);
+            File methodResult = null;
+            try {
+            methodResult = MyClass.fileSort(file, Comparator.naturalOrder(), 4);
+            } catch (IOException ex) {
+                fail(FAIL_MESSAGE_FILESORT);
+            }
             File result = new File(PATH + "result.csv");
-            assertTrue("The files differ result 3!", FileUtils.contentEquals(methodResult, result));
+            assertTrue("The files differ result BiggerNoMultiple!", FileUtils.contentEquals(methodResult, result));
             Files.delete(methodResult.toPath());
         } catch (IOException ex) {
-            fail();
+            fail(FAIL_MESSAGE_FILES_PROCESSING);
         }
     }
 
@@ -88,10 +115,15 @@ public class TestMyClass {
     public void testBigFileSort() {
         try {
             File file = new File(PATH + "bigdata.csv");
-            File methodResult = MyClass.fileSort(file, Collections.reverseOrder(), 10000);
+            File methodResult = null;
+            try {
+            methodResult = MyClass.fileSort(file, Collections.reverseOrder(), 10000);
+            } catch (IOException ex) {
+                fail(FAIL_MESSAGE_FILESORT);
+            }
             Files.delete(methodResult.toPath());
         } catch (IOException ex) {
-            fail();
+            fail(FAIL_MESSAGE_FILES_PROCESSING);
         }
     }
 
@@ -99,11 +131,15 @@ public class TestMyClass {
     public void testEmptyFileSort() {
         try {
             File file = new File(PATH + "emptyFile.csv");
-            File methodResult = MyClass.fileSort(file, Comparator.reverseOrder(), 6);
-            File result = new File(PATH + "emptyFile.csv");
-            assertTrue("The files differ emptyFile!", FileUtils.contentEquals(methodResult, result));
+            File methodResult = null;
+            try {
+            methodResult = MyClass.fileSort(file, Comparator.reverseOrder(), 6);
+            } catch (IOException ex) {
+                fail(FAIL_MESSAGE_FILESORT);
+            }
+            assertTrue("The files differ emptyFile!", FileUtils.contentEquals(methodResult, file));
         } catch (IOException ex) {
-            fail();
+            fail(FAIL_MESSAGE_FILES_PROCESSING);
         }
     }
 
